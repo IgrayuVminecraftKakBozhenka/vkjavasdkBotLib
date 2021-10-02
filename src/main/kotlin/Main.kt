0dcdfbc7c4d829022ca21bccec2c315c3516ad87
IgrayuVminecraftKakBozhenka <danko200028@mail.ru>
2021-10-02 19:09:44 +0600

    core

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
+import com.vk.api.sdk.client.VkApiClient
+import com.vk.api.sdk.client.actors.GroupActor
+import com.vk.api.sdk.httpclient.HttpTransportClient
+
+var groupId: Int = 0
+var accessToken: String = ""
+val actor = GroupActor(203148566,"b99c377d206b1528a1ce0ceba00c195e2ccafd6b3aee991e1e213522d013129e2c0a814290346fe5485fa")
+val vkApiClient =  VkApiClient(HttpTransportClient.getInstance())
+val ts = vkApiClient.messages().getLongPollServer(actor).execute().ts
+
 fun main() {
-    print("")
+    val core = Core(4234234, "")
 }
